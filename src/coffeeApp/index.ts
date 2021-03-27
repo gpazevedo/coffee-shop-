0042143f66bb2d82359bc73dbfc030d5d99674f8
Gustavo Azevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2021-03-27 08:36:45 -0300

    feat: Balance for each User

--- src/coffeeApp/index.ts
+++ src/coffeeApp/index.ts
@@ -1,19 +1,23 @@
 import { loadPrices} from '@/loadPrices'
 import { loadOrders } from '@/loadOrders'
+import { loadPayments } from '@/loadPayments'
 import { definePrices } from '@/prices'
-import { Order, Balance } from '@/coffee'
+import { Order, Payment, Balance } from '@/coffee'
 import { ordersToBalance } from '@/ordersToBalance';
+import { paymentsToBalance } from '@/paymentsToBalance'
 import { amount } from '@/amount'
 
 export function coffeeApp(prices: any[], orders: any[], payments: any[]): { user: string, order_total: number, payment_total: number }[] {
 
   const price = definePrices(loadPrices(prices));
   const ordered: Order[] = loadOrders(orders)
+  const paid: Payment[] = loadPayments(payments)
 
   const noBalance: Map<string, Balance> = new Map()
   const balanceFromOrders = ordersToBalance(noBalance, ordered, price)
+  const balanceFromPayments = paymentsToBalance(balanceFromOrders, paid)
 
-  const balances = Array.from(balanceFromOrders.values()).map((balance) => {
+  const balances = Array.from(balanceFromPayments.values()).map((balance) => {
     return {
       balance: amount(balance.ordered - balance.paid),
         user: balance.account, order_total: balance.ordered, payment_total: balance.paid
@@ -21,4 +25,3 @@ export function coffeeApp(prices: any[], orders: any[], payments: any[]): { user
   })
   return balances
 }
-
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ import { coffeeApp } from '@/coffeeApp'
 
 const pricesJSON = './data/prices.json'
 const ordersJSON = './data/orders.json'
+const paymentsJSON = './data/payments.json'
 
-const balances = coffeeApp(readJSON(pricesJSON), readJSON(ordersJSON), [])
+const balances = coffeeApp(readJSON(pricesJSON), readJSON(ordersJSON), readJSON(paymentsJSON))
 
 balances.forEach((balance) => {
   console.log(balance)
