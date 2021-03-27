50bb0f274ba57780454ce6d47f08e69d71500fac
Gustavo Azevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2021-03-27 09:53:18 -0300

    Command Line

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
+#!/usr/bin/env node
+
 import 'module-alias/register'
+import { program } from 'commander'
 import { readJSON } from '@/readJSON'
-import {writeJSON} from '@/writeJSON'
+import { writeJSON } from '@/writeJSON'
 import { coffeeApp } from '@/coffeeApp'
 
+program
+  .version('0.0.1')
+  .description("Coffee shop balance")
+  .option('-, --path <name>', 'data folder where orders.json, payments.json and prices.json is located')
+  .parse(process.argv);
 
-const pricesJSON = './data/prices.json'
-const ordersJSON = './data/orders.json'
-const paymentsJSON = './data/payments.json'
-const balanceJSON = './data/payments.json'
+const options = program.opts()
+const path = options.path ? options.path : './data'
+
+const pricesJSON = path + '/prices.json'
+const ordersJSON = path + '/orders.json'
+const paymentsJSON = path + '/payments.json'
+const balanceJSON = path + '/payments.json'
 
 const balances = coffeeApp(readJSON(pricesJSON), readJSON(ordersJSON), readJSON(paymentsJSON))
 
